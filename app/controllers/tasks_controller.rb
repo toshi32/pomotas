73526db12a785ecd4d5dbf6dba2582fa1907fb47
toshi32 <viva.la.vida0522@gmail.com>
2021-06-18 11:41:57 +0900

    追加:Task Controller/View作成、Task root設定

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+  def task_params
+  end
+
+  def set_task
+  end
+end
